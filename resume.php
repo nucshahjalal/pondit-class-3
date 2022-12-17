ee224dc455068c5f6a01acaa642ea6a625ba8af9
nucshahjalal <76701175+nucshahjalal@users.noreply.github.com>
2022-12-17 22:55:31 +0600

    Add files via upload

---
+++ resume.php
@@ -0,0 +1,179 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Resume Letter</title>
+</head>
+<style>
+		
+		table {
+		  font-family: arial, sans-serif;
+		  border-collapse: collapse;
+		  width: 50%;
+		}
+
+		td, th {
+		  border: 1px solid #dddddd;
+		  text-align: left;
+		  padding: 8px;
+		}
+
+		tr:nth-child(even) {
+		  background-color: #dddddd;
+		}
+
+
+		* {
+			  box-sizing: border-box;
+			}
+
+		
+		.column {
+		  float: left;
+		  width: 50%;
+		  padding: 5px;
+		  height: 200px; 
+		}
+
+		
+		.row:after {
+		  content: "";
+		  display: table;
+		  clear: both;
+		}
+
+
+		.content{
+			margin-left: 5%;
+		}
+		
+		.photo {	
+		  width: 180px;
+		   height: 180px;
+		  border: 1px solid gray;
+		  padding: 20px;
+		  margin: 15px;
+		}
+
+		p{
+			font-size: 20px;
+		}
+
+</style>
+<body>
+	<div class="content">
+		<div class="row">
+			<div class="column" style="">
+			    <h1>Curriculum Vitae of <br>MD.Shah Jalal</h1>
+                <h3>Address:Panchthubi,Laksam,Cumilla</h3>
+                <h3>Mobile: 01814-739712</h3>
+                <h3>E-Mail: msjalalc36@gmail.com</h3>
+			</div>
+
+		  	<div class="column">
+			    <div class="photo">
+			    	<img src="images/jalal.jpg" alt="" width="140" height="140">
+			    </div>
+		    </div>
+		</div>
+		
+	    <br><br>
+		<div>
+			<h1>Career Objective:</h1>
+			<p>To explore a challenging and rewarding position at a winning organization where creativity, <br>skill and performance are the criteria for ones appraisal and recognition and where I will be<br> able to utilize my expertise for the development of the company. 
+			</p>
+		</div>
+		<div>
+			<h1>Employment History:</h1>
+			<p><strong>Programmer Codeigniter4</strong> ( May  01, 2021  – Continue)</p>
+			<p><strong>Total Year of Experience :</strong> 1.6 Year(s)</p>
+			<p><strong>Codetroopers Comphany(COMPAHNY)</strong></p>
+			<p><strong>Programmer Oracle ( Mar  10, 2019  – to 30 June 2020)</strong></p>
+			<p><strong>Jubosoft Information Systems PVT (Ltd).</strong></p>
+			<h1>Duties/Responsibilities:</h1>
+			<p>Worked as Full Supporting, Customization and Software Development.<br>
+				Zone Operation Center From 10 Mar 2019 to 30 June 2020.<br>
+				Worked as Testing and Implementation at Jubosoft Information Systems PVT (Ltd) at Farmgate-Dhaka from<br> work on Database.<br>
+				Testing on VAT Management Systems Software for NBR Law.
+			</p>
+		</div>
+
+		<div>
+			<h1>Academic Qualification:</h1>
+			<table>
+				<tr>
+					<th>Course</th>
+					<th>Institutions</th>
+					<th>Duration</th>
+					<th>Passing</th>
+					<th>Grade/Class</th>
+				</tr>
+				<tr>
+					<td>BSS Ohs</td>
+					<td>Cumilla Victoria Gov.College</td>
+					<td>4 Years</td>
+					<td>2017</td>
+					<td>3.10</td>
+				</tr>
+			</table>
+		</div>
+
+		<div>
+			<h1>Database Skills:</h1>
+			<p>Install and Configure Oracle 11g database, Export/Import with Data pump, Flashback, Database Auditing,<br> Backup and Restoration. </p>
+			<p>* Oracle 11G: SQL,MY SQL, PL/SQL, Forms, Reports.<br>
+			* SQL, PL/SQL Programming Languages.
+			</p>
+			<h1>IT Skills:</h1>
+			<address>Professional Qualification:   IT Course in Diploma from IDB-BISEW.<br>
+			Course Title: Database Design & Development (DDD).<br>
+			Codeigniter4 : Codetroopers Comphny (Comphny)<br>
+			Laravel : Laravel Basic,Practice Continue
+			</address>
+		</div>
+
+		<div>
+			<h1>Personal Statement:</h1>
+			<table>
+				<tr>
+					<th>Name</th>
+					<td>Md. Shah Jalal</td>
+				</tr>
+				<tr>
+					<th>Gender</th>
+					<td>Male</td>
+				</tr>
+				<tr>
+					<th>E-mail</th>
+					<td>msjalalc36@gmail.com</td>
+				</tr>
+				<tr>
+					<th>Contact#</th>
+					<td>01814739712</td>
+				</tr>
+				<tr>
+					<th>Father</th>
+					<td>MD. Ozi Ullah</td>
+				</tr>
+				<tr>
+					<th>Mother</th>
+					<td>Razia Begum</td>
+				</tr>
+				<tr>
+					<th>Address</th>
+					<td>Vill.Zonarddanpur, P.O.Panchthubi,P.S.Laksam,Dis.Cumilla</td>
+				</tr>
+			</table>
+		</div>
+
+		<div>
+			<h1>Certification:</h1>
+			<p>"I certify that all information stated in this resume is true and complete to the best of my knowledge.<br> I hereby authorize the respective concern to verify the information provided in this resume."  </p>
+			<span>..................</span><br>
+			<span>Signature</span><br>
+			<span>Date : ........./........./..........</span>
+		</div>
+
+	</div>
+</body>
+</html>
+
